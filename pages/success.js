17bd85d1ff2dcb66bca1ad9bf1e3acded79ec319
Cl1PZ <mamajoe69420m@gmail.com>
2026-05-04 17:07:51 -0500

    Create success.js

---
+++ pages/success.js
@@ -0,0 +1,9 @@
+export default function Success() {
+  return (
+    <div style={{color:"white", textAlign:"center", marginTop:"100px"}}>
+      <h1>Payment Successful</h1>
+      <p>Your key has been generated.</p>
+      <a href="/dashboard">Go to Dashboard</a>
+    </div>
+  );
+}
